dd5f11a7f126949d409c3c4aa966cf09f2764dc3
AJAmit17 <amitjagadeesh2004@gmail.com>
2024-01-27 13:44:49 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/navbar";
+
+export default function Home() {
+  return (
+    <main className="flex items-center flex-col justify-center p-2">
+      <Navbar/>
+    </main>
+  );
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ModeToggle } from './toggleMode'
+
+const Navbar = () => {
+  return (
+    <div>Navbar
+        <ModeToggle/>
+    </div>
+  )
+}
+
+export default Navbar
