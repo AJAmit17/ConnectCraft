b4e7b22c3747532b76ad2fedb2331f4994a2470c
AJAmit17 <amitjagadeesh2004@gmail.com>
2024-01-31 21:53:24 +0530

    Fix: Minor fix

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,16 @@ const nextConfig = {
     serverComponentsExternalPackages: ["mongoose"],
   },
   images: {
-    domains: ["example.com"],
+    remotePatterns: [
+      {
+        protocol : "https",
+        hostname : "*",
+      },
+      {
+        protocol : "http",
+        hostname : "*",
+      }
+    ]
   },
 };
 
