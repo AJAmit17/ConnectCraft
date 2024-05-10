33e3408449a45e0020e2b912cd571d4a5e7f201c
AJAmit17 <amitjagadeesh2004@gmail.com>
2024-05-11 00:04:19 +0530

    Add:Footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,25 +1,26 @@
 import Navbar from "@/components/navbar";
 import LeftSideBar from "@/components/leftSidebar";
 import RightSideBar from "@/components/rightSidebar";
+import Footer from "@/components/footer";
 
-export default function AuthLayout({
-    children
+export default function HomeLayout({
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode;
 }) {
-    return (
-        <main>
-            <Navbar />
-            <div className="flex relative">
-                <LeftSideBar />
-                <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-mb:pb-14 sm:px-14">
-                    <div className="mx-auto w-full max-w-5xl">
-                        {children}
-                    </div>
-                </section>
-                <RightSideBar />
-            </div>
-            {/* Toasters */}
-        </main>
-    );
-};
+  return (
+    <main className="flex flex-col h-screen">
+      <Navbar />
+      <div className="flex flex-1">
+        <LeftSideBar />
+        <section className="flex-1 flex flex-col px-6 pb-6 pt-36 max-mb:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">
+            {children}
+            <Footer />
+          </div>
+        </section>
+        <RightSideBar />
+      </div>
+    </main>
+  );
+}
